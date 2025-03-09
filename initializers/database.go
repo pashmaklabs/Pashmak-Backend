8b50e8dce036aea012278c46c202cc6df9162a86
saeednourian82 <saeednourian82@gmail.com>
2025-03-09 17:42:29 +0330

    refactor: Declare DB as a global variable for accesbility #8

--- initializers/database.go
+++ initializers/database.go
@@ -14,8 +14,10 @@ type DatabaseDSN struct {
 	port		string
 }
   
+var DB *gorm.DB
 
 func SetUpPostgres() *gorm.DB {
+	var err error
 	postgres_dsn := DatabaseDSN{
 		host: POSTGRES_HOST, 
 		user: POSTGRES_USER,
@@ -25,10 +27,10 @@ func SetUpPostgres() *gorm.DB {
 	}	
 	var dsn string = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Tehran",
 									postgres_dsn.host, postgres_dsn.user, postgres_dsn.password, postgres_dsn.dbname, postgres_dsn.port)
-	db, err := gorm.Open(postgres.Open(dsn),  &gorm.Config{})
+	DB, err = gorm.Open(postgres.Open(dsn),  &gorm.Config{})
 	if err != nil {
 		// TODO: set logger instead of Println
 		fmt.Println("failed to initialize database")
 	}
-	return db
+	return DB
 }
