a92b95f828e6aaeefb150fa284365b75ebafd568
saeednourian82 <saeednourian82@gmail.com>
2025-03-10 06:26:29 +0330

    feature: Add auth service #8

--- authentication/services.go
+++ authentication/services.go
@@ -1,16 +1,39 @@
 package authentication
 
-type AuthService struct{}
+import (
+  "pashmak.com/pashmak/initializers"
+  "pashmak.com/pashmak/models"
+  "gorm.io/gorm"
+  "math/rand"
+  "time"
+  "fmt"
+)
+
+type AuthService struct{
+  DB *gorm.DB
+}
 
 func NewAuthService() *AuthService{
-	return &AuthService{}
+  return &AuthService{}
+}
+
+func GnerateOTP()string{
+  rand.NewSource(time.Now().UnixNano())
+    otp := rand.Intn(10000)
+    return fmt.Sprintf("%04d", otp)
 }
 
 func CheckExistance(email string) bool{
-	
-	return false
+  var user models.User
+  initializers.DB.First(&user, "email = ?", email)
+  return user.ID != 0
 }
 
-func (as *AuthService)ValidateUser(email string){
-	
+func (as *AuthService)ValidateUser(email string) bool{
+  if CheckExistance(email){
+    userotp := GnerateOTP()
+    fmt.Println(userotp)
+    return true
+  }
+  return false
 }
