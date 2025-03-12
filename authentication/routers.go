9e49f6f760d22ec87ed5d1316a2fd8e74b68c62a
saeednourian82 <saeednourian82@gmail.com>
2025-03-12 14:55:25 +0330

    refactor: Change router and controller logic for modularity and testability #8

--- authentication/routers.go
+++ authentication/routers.go
@@ -1,15 +1,31 @@
 package authentication
 
 import (
+	"log"
 	"github.com/gin-gonic/gin"
+	"pashmak.com/pashmak/initializers"
 )
 
-var(
-	routeService = NewAuthService()
-	routeController = NewAuthController(routeService)
+var (
+	authService    = NewAuthService(initializers.DB, initializers.RedisClient)
+	authController = NewAuthController(authService)
 )
+
+// func init() {
+// 	log.Println("Initializing AuthService...")
+
+// 	if authService.DB == nil {
+// 		log.Println("Warning: authService is nil")
+// 		log.Println(initializers.DB)
+// 		log.Println(initializers.RedisClient)
+// 		panic("err")
+// 	}
+
+// }
 
 func AuthRoutes(router *gin.Engine) {
 	//Add routes here
-	router.POST("StartEmailAuth", routeController.StartEmailAuth)
+	log.Println(initializers.DB)
+	log.Println(initializers.RedisClient)
+	router.POST("/StartEmailAuth", authController.StartEmailAuth)
 }
--- authentication/services.go
+++ authentication/services.go
@@ -6,39 +6,58 @@ import (
 	"math/rand"
 	"time"
 
-	// "github.com/redis/go-redis/v9"
+	"github.com/redis/go-redis/v9"
 	"gorm.io/gorm"
 	"pashmak.com/pashmak/initializers"
 	"pashmak.com/pashmak/models"
 )
 
 type AuthService struct{
-  DB *gorm.DB
+  DB          *gorm.DB
+  RedisClient *redis.Client
 }
 
-func NewAuthService() *AuthService{
-  return &AuthService{}
+func NewAuthService(db *gorm.DB, redisClient *redis.Client) *AuthService{
+  return &AuthService{
+    DB : db,
+    RedisClient: redisClient,
+  }
 }
 
-func GnerateOTP()string{
+func GenerateOTP()string{
   rand.NewSource(time.Now().UnixNano())
   otp := rand.Intn(10000)
   return fmt.Sprintf("%04d", otp)
 }
 
-func CheckExistance(email string) bool{
+func (as *AuthService)CheckExistance(email string) (bool, error){
   var user models.User
-  initializers.DB.First(&user, "email = ?", email)
-  return user.ID != 0
+  fmt.Println(as.DB)
+  // FIXME: initializers.DB should be replaced with as.DB but it causes error
+  result := initializers.DB.First(&user, "email = ?", email)
+  
+  if result.Error != nil{
+    if result.Error == gorm.ErrRecordNotFound{
+      return false, nil
+    }
+    return false, result.Error
+  }
+  return user.ID != 0, nil
 }
 
 func (as *AuthService)ValidateUser(email string) (bool, error){
-  if CheckExistance(email){
-    userotp := GnerateOTP()
+  exists, err := as.CheckExistance(email)
+  if err != nil{
+    return false, fmt.Errorf("failed to check user existence: %w", err)
+  }
+
+  if exists{
+    userOTP := GenerateOTP()
     ctx := context.Background()
-    err := initializers.RedisClient.Set(ctx, email, userotp, 5*time.Minute).Err()
+    // FIXME: initializers.RedisClient should be replaced with as.RedisClient but it causes error
+    err = initializers.RedisClient.Set(ctx, email, userOTP, 5*time.Minute).Err()
     if err != nil{
-      return true, err
+      return true, fmt.Errorf("failed to set OTP in Redis: %w", err)
     }
     return true, nil
   }
