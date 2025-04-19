9e9a1d85c811ae2dbf6799fdff307a93bf7c4faa
saeednourian82 <saeednourian82@gmail.com>
2025-04-20 02:14:17 +0330

    feature: Implement navigation logic #17

--- controllers/navigation/navigation.go
+++ controllers/navigation/navigation.go
@@ -1,6 +1,9 @@
 package controllers_navigation
 
 import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 	"pashmak.com/pashmak/bootstrap"
 	services_navigation "pashmak.com/pashmak/services/navigation"
@@ -18,4 +21,24 @@ func NewNavigationController(navigationService *services_navigation.NavigationSe
 		DB:                db,
 		AppConfig:         appConfig,
 	}
+}
+
+func (rc *NavigationController) GetRoute(c *gin.Context) {
+	startLat := c.Query("start_lat")
+	startLon := c.Query("start_lon")
+	endLat := c.Query("end_lat")
+	endLon := c.Query("end_lon")
+
+	if startLat == "" || startLon == "" || endLat == "" || endLon == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Missing parameters"})
+		return
+	}
+
+	route, err := rc.NavigationService.FetchRoute(startLat, startLon, endLat, endLon)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, route)
 }
--- server.go
+++ server.go
@@ -9,6 +9,7 @@ import (
 	"pashmak.com/pashmak/bootstrap"
 	middlewares_cors "pashmak.com/pashmak/middlewares/cors"
 	routers_auth "pashmak.com/pashmak/routers/auth"
+	routers_navigation "pashmak.com/pashmak/routers/navigation"
 	routers_profile "pashmak.com/pashmak/routers/profile"
 )
 
@@ -35,6 +36,7 @@ func main() {
 	// Add each domain routes here
 	routers_auth.AuthRoutes(Router, DB, Redis, AppConfig)
 	routers_profile.ProfileRoutes(Router, DB, Redis, AppConfig)
+	routers_navigation.NavigationRoutes(Router, DB, AppConfig)
 
 	Router.Run(fmt.Sprintf(":%s", AppConfig.ServerPort))
 }
