038db05ca8285c2c269f3abc11255b2ae2f6ceae
saeednourian82 <saeednourian82@gmail.com>
2025-06-10 18:57:18 +0000

    refactor: Delete commented-out code #32
    See merge request tahlil/backend/core!74

--- serializers/navigation/navigation.go
+++ serializers/navigation/navigation.go
@@ -1,27 +1,14 @@
 package serializers_navigation
 
 type WaypointsResponse struct {
-	// INFO: Commented lines will be deleted after confidence
 	Routes []struct {
 		Geometry struct {
 			Coordinates [][]float64 `json:"coordinates"`
-			// Type        string      `json:"type"`
 		} `json:"geometry,omitempty"` // Use omitempty to handle cases where geometry is not present
-		// Legs []struct {
-		// 	Steps    []interface{} `json:"steps"`
-		// 	Summary  string        `json:"summary"`
-		// 	Weight   float64       `json:"weight"`
-		// 	Duration float64       `json:"duration"`
-		// 	Distance float64       `json:"distance"`
-		// } `json:"legs"`
-		// WeightName string  `json:"weight_name"`
-		// Weight     float64 `json:"weight"`
 		Duration   float64 `json:"duration"`
 		Distance   float64 `json:"distance"`
 	} `json:"routes"`
 	Waypoints []struct {
-		// Hint     string    `json:"hint"`
-		// Distance float64   `json:"distance"`
 		Name     string    `json:"name"`
 		Location []float64 `json:"location"`
 	} `json:"waypoints"`
