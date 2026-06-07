b5e1a16faa1f6b11aefba8a2ec253b42c272df80
saeednourian82 <saeednourian82@gmail.com>
2026-06-07 20:09:26 +0330

    feature: Apply rate limit middleware to place

--- routers/place/place.go
+++ routers/place/place.go
@@ -1,6 +1,8 @@
 package routers_place
 
 import (
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/minio/minio-go/v7"
 	"github.com/redis/go-redis/v9"
@@ -8,6 +10,7 @@ import (
 	"pashmak.com/pashmak/bootstrap"
 	controllers_place "pashmak.com/pashmak/controllers/place"
 	middlewares_auth "pashmak.com/pashmak/middlewares/auth"
+	middlewares_ratelimit "pashmak.com/pashmak/middlewares/ratelimit"
 	middlewares_validation "pashmak.com/pashmak/middlewares/validation"
 	serializers_place "pashmak.com/pashmak/serializers/place"
 	services_auth "pashmak.com/pashmak/services/auth"
@@ -17,23 +20,36 @@ import (
 	services_profile "pashmak.com/pashmak/services/profile"
 )
 
-func PlaceRoutes(router *gin.Engine, db *gorm.DB, pgvectorDB *gorm.DB, redis *redis.Client, minio *minio.Client, appConfig *bootstrap.AppConfig) {
+func PlaceRoutes(router *gin.Engine, db *gorm.DB, pgvectorDB *gorm.DB, redisClient *redis.Client, minioClient *minio.Client, appConfig *bootstrap.AppConfig) {
 	openaiService := services_openai.NewOpenAIService(appConfig.OpenaiApiKey)
-	placeService := services_place.NewPlaceService(db, appConfig, openaiService, minio, pgvectorDB)
+	placeService := services_place.NewPlaceService(db, appConfig, openaiService, minioClient, pgvectorDB)
 	commentService := services_comment.NewCommentService(db, pgvectorDB, appConfig)
-	profileService := services_profile.NewProfileService(db, minio, appConfig)
+	profileService := services_profile.NewProfileService(db, minioClient, appConfig)
 	placeController := controllers_place.NewPlaceController(placeService, commentService, profileService, appConfig)
-	authService := services_auth.NewAuthService(db, redis, appConfig)
+	authService := services_auth.NewAuthService(db, redisClient, appConfig)
 	authMiddleware := middlewares_auth.NewAuthMiddleware(authService)
+
+	searchLimiter := middlewares_ratelimit.NewRateLimiter(redisClient, 30, time.Minute, "place_search", middlewares_ratelimit.KeyByIP)
+
+	getPlaceLimiter := middlewares_ratelimit.NewRateLimiter(redisClient, 120, time.Minute, "place_get", middlewares_ratelimit.KeyByIP)
+
+	imageUploadLimiter := middlewares_ratelimit.NewRateLimiter(redisClient, 20, time.Minute, "place_image_upload", middlewares_ratelimit.KeyByIP)
+
+	addPlaceLimiter := middlewares_ratelimit.NewRateLimiter(redisClient, 10, 10*time.Minute, "place_add", middlewares_ratelimit.KeyByIP)
+
+	recommendLimiter := middlewares_ratelimit.NewRateLimiter(redisClient, 30, time.Minute, "place_recommend", middlewares_ratelimit.KeyByIP)
 
 	place := router.Group("/places")
 	{
-		place.GET("/:id", authMiddleware.AuthOrAnonMiddleware(), placeController.GetPlace)
-		place.GET("/", authMiddleware.AuthOrAnonMiddleware(), placeController.SearchPlace)
-		place.GET("/recommendations", placeController.GetPlaceRecommendations)
-		place.POST("/:id/images", placeController.UploadPlaceImage)
-		place.GET("/:id/images/:image_name", placeController.GetPlaceImage)
+		place.GET("/:id", getPlaceLimiter.Middleware(), authMiddleware.AuthOrAnonMiddleware(), placeController.GetPlace)
+		place.GET("/", searchLimiter.Middleware(), authMiddleware.AuthOrAnonMiddleware(), placeController.SearchPlace)
+		place.GET("/recommendations", recommendLimiter.Middleware(), placeController.GetPlaceRecommendations)
+
+		place.POST("/:id/images", imageUploadLimiter.Middleware(), placeController.UploadPlaceImage)
+		place.GET("/:id/images/:image_name", getPlaceLimiter.Middleware(), placeController.GetPlaceImage)
+
 		place.POST("/new_place",
+			addPlaceLimiter.Middleware(),
 			middlewares_validation.ValidationMiddleware[serializers_place.AddPlaceRequest](),
 			authMiddleware.LoginMiddleware(),
 			placeController.AddNewPlace)
